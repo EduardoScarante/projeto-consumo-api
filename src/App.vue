9f102feecb9c2c3d08814fbb62e6bd235f67f611
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-06 09:26:13 -0300

    primeiros arquivos

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+/* nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+} */
+
+</style>
