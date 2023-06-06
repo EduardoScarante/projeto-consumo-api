fe32202c45fdf36fe0c8b6088727db78e43121a2
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-06 11:49:34 -0300

    busca imagens api google ok

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,63 +1,58 @@
 <script>
-export default{
-    data(){
-        return{
-            a: this.$route.params.name
-        }
-    }
-}
-
-</script>
-
-<template>
-   {{ a }}
-</template>
-
-<!-- <script>
 export default {
-  data() {
-    return {
-      loading: false,
-      imageUrl: "", //Propriedade para armazenar a URL da imagem
-    };
-  },
-  methods: {
-    getImages(url) {
-      this.loading = true;
+    data() {
+        return {
+            loading: false,
+            imageUrl: [],
+        };
+    },
+    methods: {
+        getImages(url) {
+            this.loading = true;
 
-          // Obtem foto do Google usando a API de pesquisa de imagens
-          const googleSearchApiKey = "AIzaSyBLq5cUwYO31kXPjCxhaELx_tNXv_TI-ec"; // Chave de API do Google JSON  com pesquisas de até 10 000 requisições por dia se for somente web é ilimitado
-          const customSearchEngineId = "163bf32740a0e4962"; // ID de pesquisa personalizado
-          const googleSearchUrl = `https://www.googleapis.com/customsearch/v1?key=${googleSearchApiKey}&cx=${customSearchEngineId}&q=${/*NOME DA VARIAVEL CIDADE*/}&searchType=image`;
+            // Obtem foto do Google usando a API de pesquisa de imagens
+            const googleSearchApiKey = "AIzaSyBLq5cUwYO31kXPjCxhaELx_tNXv_TI-ec"; // Chave de API do Google JSON  com pesquisas de até 10 000 requisições por dia se for somente web é ilimitado
+            const customSearchEngineId = "163bf32740a0e4962"; // ID de pesquisa personalizado
+            const googleSearchUrl = `https://www.googleapis.com/customsearch/v1?key=${googleSearchApiKey}&cx=${customSearchEngineId}&q=${url}&searchType=image`;
 
-          fetch(googleSearchUrl)
-            .then((res) => res.json())
-            .then((data) => {
-              if (data.items && data.items.length > 0) {
-                const imageUrl = data.items[1].link;
-                this.imageUrl = imageUrl;
-              }
-              console.log(data.items);
-            });
-
-          this.loading = false;
+            fetch(googleSearchUrl)
+                .then((res) => res.json())
+                .then((data) => {
+                    for (let x = 0; x < 10; x++) {
+                        this.imageUrl.push(data.items[x].link)
+                    }
+                });
+            this.loading = false
         },
     },
-  };
-  mounted() {
-    const id = this.$route.params.id; // acho que é assim
-    this.getImages(`
-    // ADIDIONAR A ROTA COM ID DA CIDADE //
-    `);
-  },
+    mounted() {
+        this.getImages(this.$route.params.name);
+    },
 };
 </script>
 
 <template>
-  <h3 v-show="loading">ALGO BONITINHO PARA APARECER QUANDO ESTÁ CARREGANDO</h3>
+    <h3 v-show="loading">ALGO BONITINHO PARA APARECER QUANDO ESTÁ CARREGANDO</h3>
 
-  <div v-if="imageUrl">
-    <img :src="imageUrl" alt="Google Image" key="API_KEY" />
-  </div>
+    <div class="car">
+        <div v-for="x of imageUrl">
+            <img :src="x" alt="Google Image" key="API_KEY" />
+            
+        </div>
+    </div>
+
 </template>
- -->
+
+<style scoped>
+img {
+    height: 250px;
+    margin: 10px;
+}
+
+.car{
+    display: flex;
+    margin: auto;
+    width: 75vw;
+    overflow-y: auto;
+}
+</style>
