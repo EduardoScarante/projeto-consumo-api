54304835fbc97e2ed2652c7a454aec443b918df1
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-07 14:39:56 -0300

    coreção box busca tela inicial

--- src/components/search.vue
+++ src/components/search.vue
@@ -12,20 +12,24 @@ export default {
 
 <template>
     <main class="content" :id="filteredCidades.nome">
-        {{ filteredCidades.nome }}
+        {{ filteredCidades.nome }} <br>
         {{ filteredCidades.estado }} / {{ filteredCidades.UF }}
     </main>
 </template>
 
 <style scoped>
 .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
     width: 500px;
     height: 100px;
     margin-bottom: 10px;
     border-radius: 5px;
     margin: 10px;
 
-    background-color: rgb(236, 236, 236);
+    background-color: rgb(236, 236, 236) !important; 
     box-shadow: 1px 1px 3px lightgray;
 }
 </style>
