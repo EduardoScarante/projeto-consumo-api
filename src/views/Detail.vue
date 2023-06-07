f1ea2ac4d5a88e3f7f4db91e0dcc68d935c00b89
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-07 11:11:45 -0300

    files

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -2,16 +2,19 @@
 
 import climaBox from '../components/climaBox.vue'
 
+
 export default {
     components: {
-        climaBox
+        climaBox,
     },
     data() {
         return {
             search: this.$route.params.name,
             loading: false,
             imageUrl: [],
-            clima: []
+            clima: [],
+            climaCarosel: [],
+            index: 0
         };
     },
     methods: {
@@ -39,32 +42,59 @@ export default {
             fetch(url)
                 .then(e => e.json())
                 .then(data => {
-                    for (let x = 0; x < 14; x++) {
+                    for (let x = 0; x < 10; x++) {
                         this.clima.push({
-                            "data": data.days[x].datetime,
-                            "tempmin": data.days[x].tempmin,
-                            "tempmax": data.days[x].tempmax,
-                            "tempmedia": data.days[x].temp,
+                            "index": x,
+                            "data": data.days[x].datetime.split("-").reverse().join("/"),
+                            "tempmin": data.days[x].tempmin + ("°"),
+                            "tempmax": data.days[x].tempmax + ("°"),
+                            "tempmedia": data.days[x].temp + ("°"),
                             "nascerdosol": data.days[x].sunrise,
                             "pordosol": data.days[x].sunset,
-                            "humidade": data.days[x].humidity,
+                            "humidade": data.days[x].humidity + "%",
                             "condição": data.days[x].conditions,
                             "descrição": data.days[x].description,
-                            "precipitação": data.days[x].precip,
+                            "precipitação": data.days[x].precip + "mm",
                             "icon": data.days[x].icon.replaceAll("-", "")
 
                         })
-                        console.log(data.days[x])
+                    }
+
+                    for (let x = 0; x < 3; x++) {
+                        this.climaCarosel.push({
+                            "index": x,
+                            "data": data.days[x].datetime.split("-").reverse().join("/"),
+                            "tempmin": data.days[x].tempmin + ("°"),
+                            "tempmax": data.days[x].tempmax + ("°"),
+                            "tempmedia": data.days[x].temp + ("°"),
+                            "nascerdosol": data.days[x].sunrise,
+                            "pordosol": data.days[x].sunset,
+                            "humidade": data.days[x].humidity + "%",
+                            "condição": data.days[x].conditions,
+                            "descrição": data.days[x].description,
+                            "precipitação": data.days[x].precip + "mm",
+                            "icon": data.days[x].icon.replaceAll("-", "")
+
+                        })
                     }
                 }).catch(err => {
                     console.log("erro: ", err)
                 })
+        },
+        indexMenos() {
+            if (this.index == 0) return
+            this.index--
+            this.climaCarosel = this.clima.slice(this.index, this.index + 3)
+        },
+        indexMais() {
+            if (this.index == 7) return
+            this.index++
+            this.climaCarosel = this.clima.slice(this.index, this.index + 3)
         }
     },
     mounted() {
-        this.getImages(this.search);
+        //this.getImages(this.search);
         this.getClima(this.search);
-
     },
 };
 </script>
@@ -74,40 +104,87 @@ export default {
 
 
     <main>
-        <div class="car">
+        <div class="caroselImg">
             <div v-for="x of imageUrl">
-                <img :src="x" alt="Google Image" key="API_KEY" />
+                <img :src="x" alt="Google Image" />
             </div>
         </div>
 
-        <div v-for="x of clima" class="climaBox">
-            <climaBox :clima="x" />
+        <div class="climaBox">
+            <div @click="indexMenos" class="arrow">
+                &lt
+            </div>
+            <div v-for="x of climaCarosel">
+                <climaBox :clima="x" />
+            </div>
+            <div @click="indexMais" class="arrow">
+                >
+            </div>
         </div>
+
     </main>
 </template>
 
 <style scoped>
-main{
+main {
     display: flex;
-    margin: auto;
-    width: 80vw;
-    flex-wrap: wrap;
-}
-
-.climaBox{
-    margin: auto;
+    flex-direction: column;
 }
 
 img {
     height: 250px;
     margin: 10px;
 }
-.car {
+
+.caroselImg {
     display: flex;
     margin: auto;
-    width: 75vw;
-    height: 150px;
-    background-color: azure;
+    width: 90vw;
+    height: 300px;
+    background-color: white;
+    box-shadow: 8px 8px 24px #c5c5c5,
+        -8px -8px 24px #fbfbfb;
     overflow-y: auto;
+    border-radius: 10px;
+}
+
+
+/* CLIMA */
+.climaBox {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: auto;
+
+    width: 80vw;
+
+}
+
+
+.arrow {
+    margin: 10px;
+
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 50px;
+
+    border-radius: 10px;
+    background: rgb(255, 255, 255, 0.2);
+    font-weight: 700;
+    font-size: 30px;
+    box-shadow: 8px 8px 24px #c5c5c5,
+        -8px -8px 24px #fbfbfb;
+
+    transition: 0.2s;
+     
+    user-select: none;
+}
+
+.arrow:hover {
+    background-color: rgb(245, 245, 245);
 }
 </style>
