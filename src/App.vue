b9caba8cac7929e4c63f101f651ad59c47daf426
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-06-08 14:52:47 -0300

    add component api love

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,25 @@
 <script>
 import { RouterLink, RouterView } from "vue-router";
-import lottie1 from "./lottie/love-dog.json";
-import lottie2 from "./lottie/love-sheep.json";
 
 import icon from "./icons/love-message.png";
 
+import loveApi from "./components/apiLove.vue"
+
 export default {
+  components:{
+    loveApi
+  },
   data() {
     return {
-      lottie1,
-      lottie2,
       icon,
-
       AnuncioApiLove: false,
-      nome1: "",
-      nome2: "",
-      resApi: "",
     };
   },
   methods: {
-    openLoveApi() {
+    HandleModalLoveApi() {
       this.AnuncioApiLove = !this.AnuncioApiLove;
-    },
-    async calculateLoveApi() {
-      const url = `https://love-calculator.p.rapidapi.com/getPercentage?sname=${this.nome1}&fname=${this.nome2}`;
-      const options = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key":
-            "311ed1e4a9msh8fb52b3af94b7cep178b83jsnaaaec4ea33b7",
-          "X-RapidAPI-Host": "love-calculator.p.rapidapi.com",
-        },
-      };
-
-      const response = await fetch(url, options);
-      const result = await response.json();
-      this.resApi = result;
-    },
+      console.log(this.AnuncioApiLove);
+    }
   },
 };
 </script>
@@ -51,47 +34,13 @@ export default {
 
     <RouterView />
 
-    <div class="anuncio" @click="openLoveApi">
+    <div class="anuncio" @click="HandleModalLoveApi">
       <img :src="icon" alt="" />
     </div>
 
-    <div v-show="AnuncioApiLove" class="modalApiLove">
-      <div class="modal">
-        <div class="inputUser">
-          <p><b>Calculadora do Amor!</b></p>
-          <div @click="openLoveApi" class="btnClose">X</div>
-          <input type="text" placeholder="Digite seu Nome" v-model="nome1" />
-          <input
-            type="text"
-            placeholder="Digite o Nome do seu Amor!"
-            v-model="nome2"
-          />
-          <button @click="calculateLoveApi">Calcular</button>
+    <loveApi v-on:closeModal="HandleModalLoveApi" v-if="AnuncioApiLove"></loveApi>
 
-          <div class="res" v-show="resApi != ''">
-            <p>{{ resApi.percentage }}%</p>
-            <p>{{ resApi.result }}</p>
-          </div>
-        </div>
-
-        <lottie-player
-          class="lt1"
-          :src="lottie1"
-          speed="1"
-          style="width: 400px; height: auto"
-          loop
-          autoplay
-        ></lottie-player>
-        <lottie-player
-          class="lt2"
-          :src="lottie2"
-          speed="1"
-          style="width: 400px; height: auto"
-          loop
-          autoplay
-        ></lottie-player>
-      </div>
-    </div>
+    
   </main>
 </template>
 
@@ -137,71 +86,5 @@ nav a.router-link-exact-active:hover {
   cursor: pointer;
 }
 
-.modalApiLove {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgb(0, 0, 0, 0.3);
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-
-  z-index: 250;
-}
-
-.inputUser {
-  flex-direction: column;
-  justify-content: space-around;
-
-  width: 400px;
-  padding: 50px;
-  background-color: white;
-  border-radius: 10px;
-
-  background-color: lightcoral;
-}
-
-.btnClose {
-  position: absolute;
-  top: 0;
-  right: 0;
-  padding: 10px;
-}
-
-input {
-  padding: 10px;
-  border: none;
-  width: 300px;
-  margin-top: 10px;
-  margin-bottom: 10px;
-
-  background-color: rgb(255, 198, 198);
-}
-
-button {
-  background-color: red;
-  padding: 20px;
-  border-radius: 10px;
-  border: none;
-  width: 100px;
-}
-
-button:hover {
-  opacity: 0.8;
-}
-
-.lt1 {
-  position: absolute;
-  bottom: 0;
-  right: 0;
-}
-.lt2 {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-}
 </style>
